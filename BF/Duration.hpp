ac5bc18d83ae05c9600444770f5406f7fd9296c8
JohnFloor <171188054+JohnFloor@users.noreply.github.com>
2025-04-07 17:48:55 +0200

    BF::Timer

--- BF/Duration.hpp
+++ BF/Duration.hpp
@@ -2,6 +2,7 @@
 
 
 #pragma once
+#include <cassert>
 #include <chrono>
 #include "BF/Definitions.hpp"
 
@@ -9,17 +10,60 @@
 namespace BF {
 
 
+// === class HighResolutionClock =======================================================================================
+
+class HighResolutionClock : public std::chrono::high_resolution_clock {
+};
+
+
+// === MeasureDuration() ===============================================================================================
+
 double MeasureDuration(auto&& callable)
 {
-	const auto t1 = std::chrono::high_resolution_clock::now();
+	const auto t1 = HighResolutionClock::now();
 	BF_FWD(callable)();
-	const auto t2 = std::chrono::high_resolution_clock::now();
+	const auto t2 = HighResolutionClock::now();
 
 	return std::chrono::duration<double>(t2 - t1).count();		// seconds
 }
 
 
+// === GetNothingDuration() ============================================================================================
+
 double GetNothingDuration();
+
+
+// === class Timer =====================================================================================================
+
+class Timer {
+public:
+	explicit Timer(double seconds) :
+		mSeconds(seconds),
+		mEndTime(HighResolutionClock::now() + std::chrono::nanoseconds(Int64(seconds * 1e9)))
+	{
+		assert(seconds >= 0.0);
+	}
+
+	Timer(const Timer&) = delete;
+	Timer(Timer&&) = delete;
+	Timer& operator=(const Timer&) = delete;
+	Timer& operator=(Timer&&) = delete;
+
+	bool HasTime() const {
+		return HighResolutionClock::now() < mEndTime;
+	}
+
+	double GetProgress() const {
+		const double remainingDuration = std::chrono::duration<double>(mEndTime - HighResolutionClock::now()).count();		// seconds
+		const double progress          = 1.0 - remainingDuration / mSeconds;
+
+		return progress <= 1.0 ? progress : 1.0;
+	}
+
+private:
+	const double						  mSeconds;
+	const HighResolutionClock::time_point mEndTime;
+};
 
 
 }	// namespace BF
--- Test/Duration.T.cpp
+++ Test/Duration.T.cpp
@@ -1,4 +1,42 @@
 #include "BF/Duration.hpp"
 
+#include <cstdio>
+#include "BF/TestUtils.hpp"
 
-// Not tested.
+
+// Not tested. Only usage examples are shown.
+
+
+// === class HighResolutionClock =======================================================================================
+
+BF_COMPILE_TIME_TEST()
+{
+	BF::HighResolutionClock::time_point t = BF::HighResolutionClock::now();
+}
+
+
+// === MeasureDuration() ===============================================================================================
+
+BF_COMPILE_TIME_TEST()
+{
+	double printfDuration = BF::MeasureDuration([] {			// seconds
+		std::printf("Example line.\n");
+	});
+}
+
+
+// === GetNothingDuration() ============================================================================================
+
+BF_COMPILE_TIME_TEST()
+{
+	double nothingDuration = BF::GetNothingDuration();			// seconds
+}
+
+
+// === class Timer =====================================================================================================
+
+BF_COMPILE_TIME_TEST()
+{
+	for (BF::Timer t(10.0); t.HasTime(); )
+		std::printf("%f %%\n", t.GetProgress() * 100);
+}

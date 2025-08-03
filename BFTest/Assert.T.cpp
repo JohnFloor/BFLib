47d72afa9e4be3589c8a91e8bbd37c4e179be254
JohnFloor <171188054+JohnFloor@users.noreply.github.com>
2025-08-03 12:59:34 +0200

    BF_ASSERT(): Tests with a bool argument, with all cv and l/r-value.

--- BFTest/Assert.T.cpp
+++ BFTest/Assert.T.cpp
@@ -17,6 +17,20 @@ namespace {
 
 // === Test cases ======================================================================================================
 
+BF_COMPILE_TIME_TEST()
+[[gsl::suppress(es.56)]]		// allow std::move on constant variable
+{
+	{ bool                b = true; BF_ASSERT(b); }
+	{ const bool          b = true; BF_ASSERT(b); }
+	{ volatile bool       b = true; BF_ASSERT(b); }
+	{ const volatile bool b = true; BF_ASSERT(b); }
+	{ bool                b = true; BF_ASSERT(std::move(b)); }
+	{ const bool          b = true; BF_ASSERT(std::move(b)); }
+	{ volatile bool       b = true; BF_ASSERT(std::move(b)); }
+	{ const volatile bool b = true; BF_ASSERT(std::move(b)); }
+}
+
+
 TEST(Assert, Good)
 {
 	BF_ASSERT(true);
@@ -29,7 +43,7 @@ TEST(Assert, Good)
 }
 
 
-TEST(Assert, BadParameters)
+BF_COMPILE_TIME_TEST()
 {
 	// These won't give the expected error message in Release build. In Release they will compile to ((void)0).
 //	BF_ASSERT();					// [CompilationError-Debug]: ImpAssert::EnsureOneBoolParameter

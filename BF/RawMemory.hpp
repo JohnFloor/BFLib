02343b76a996ddd2e0f93923836fce7f6eb75123
JohnFloor <171188054+JohnFloor@users.noreply.github.com>
2025-08-11 16:56:50 +0200

    Revert "BF::RawRange: Not too useful class."

--- BF/RawMemory.hpp
+++ BF/RawMemory.hpp
@@ -3,9 +3,6 @@
 
 #pragma once
 #include <memory>
-#include "BF/Assert.hpp"
-#include "BF/BasicMath.hpp"
-#include "BF/ClassUtils.hpp"
 #include "BF/TypeTraits.hpp"
 
 
@@ -46,66 +43,6 @@ private:
 	void (*mFunPtr)();
 	void*  mObjPtr;
 };
-
-
-// === RawRange ========================================================================================================
-
-template <std::size_t Size, std::size_t Align>
-requires (Size != 0 && IsPowerOf2(Align) && Size % Align == 0)
-class RawRange final : ImmobileClass {
-private:
-	template <class I>
-	constexpr static bool Divides = (Size % sizeof(I) == 0) && (Align % alignof(I) == 0);
-
-	static auto ChooseInt() {
-		if      constexpr (Divides<UInt64>) return UInt64();
-		else if constexpr (Divides<UInt32>) return UInt32();
-		else if constexpr (Divides<UInt16>) return UInt16();
-		else                                return UInt8();
-	}
-
-	using Int = decltype(ChooseInt());
-
-public:
-	RawRange()       = delete;		// use 'BF::AsRawRange()' instead
-	RawRange(auto&&) = delete;		// use 'BF::AsRawRange()' instead
-
-	Int*                         begin()                             { return mArray;             }
-	const Int*                   begin() const                       { return mArray;             }
-
-	Int*                         end()                               { return mArray + GetSize(); }
-	const Int*                   end() const                         { return mArray + GetSize(); }
-
-	constexpr static std::size_t GetSize()                           { return sizeof(mArray) / sizeof(mArray[0]); }
-
-	Int&                         operator[](std::size_t index)       { BF_ASSERT(index < GetSize()); return mArray[index]; }
-	const Int&                   operator[](std::size_t index) const { BF_ASSERT(index < GetSize()); return mArray[index]; }
-
-private:
-	alignas(Align) Int mArray[Size / sizeof(Int)];
-};
-
-
-template <class... Types>
-RawRange(Types&&...) -> RawRange<1, 1>;
-
-
-template <class Type>
-auto& AsRawRange(Type& value)
-{
-	constexpr bool ConstDecayedOrArray = IsDecayedOrArray<std::remove_const_t<Type>>;
-	constexpr bool TriviallyCopyable   = ConstDecayedOrArray BF_IMPLIES std::is_trivially_copyable_v<Type>;
-	constexpr bool UniqueReps          = TriviallyCopyable BF_IMPLIES std::has_unique_object_representations_v<Type>;
-
-	static_assert(ConstDecayedOrArray, "'Type' must be decayed/const, or a bounded array of decayed/const types.");
-	static_assert(TriviallyCopyable,   "'Type' must be trivially copyable.");
-	static_assert(UniqueReps,          "A value of 'Type' can be represented by two distinct bit patterns. "
-									   "E.g., it has paddings or a floating point member.");
-
-	using CVRawRange = std29::copy_cv_t<Type, RawRange<sizeof(Type), alignof(Type)>>;
-
-	return reinterpret_cast<CVRawRange&>(value);
-}
 
 
 // === IsNullReference =================================================================================================

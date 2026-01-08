7d616d481a3ac1afb7607b3f4036e798f033ac8c
JohnFloor <171188054+JohnFloor@users.noreply.github.com>
2026-01-08 13:48:25 +0100

    BF_VERIFY(), BF_ERROR().

--- BF/Assert.hpp
+++ BF/Assert.hpp
@@ -3,6 +3,7 @@
 
 #pragma once
 #include <cassert>
+#include "BF/Definitions.hpp"
 
 
 namespace ImpAssert {
@@ -13,3 +14,12 @@ namespace ImpAssert {
 
 #define BF_ASSERT(...)	assert(ImpAssert::EnsureOneBoolParameter(__VA_ARGS__))
 #define BF_BREAK()		assert(false)
+
+
+#if BF_DEBUG
+	#define BF_VERIFY(...)	(ImpAssert::EnsureOneBoolParameter(__VA_ARGS__) ? (true)             : (BF_BREAK(), false))
+	#define BF_ERROR(...)	(ImpAssert::EnsureOneBoolParameter(__VA_ARGS__) ? (BF_BREAK(), true) : (false)            )
+#else
+	#define BF_VERIFY(...)	(ImpAssert::EnsureOneBoolParameter(__VA_ARGS__))
+	#define BF_ERROR(...)	(ImpAssert::EnsureOneBoolParameter(__VA_ARGS__))
+#endif
--- BFTest/Assert.T.cpp
+++ BFTest/Assert.T.cpp
@@ -8,10 +8,8 @@
 // === Helpers =========================================================================================================
 
 namespace {
-	template <class...>
-	struct True {
-		static constexpr bool Value = true;
-	};
+	template <class...> struct True  { static constexpr bool Value = true;  };
+	template <class...> struct False { static constexpr bool Value = false; };
 }
 
 
@@ -19,14 +17,19 @@ namespace {
 
 BF_COMPILE_TIME_TEST()
 {
-	{ bool                b = true; BF_ASSERT(b); }
-	{ const bool          b = true; BF_ASSERT(b); }
-	{ volatile bool       b = true; BF_ASSERT(b); }
-	{ const volatile bool b = true; BF_ASSERT(b); }
-	{ bool                b = true; BF_ASSERT(BF::Move(b)); }
-	{ const bool          b = true; BF_ASSERT(BF::Move(b)); }
-	{ volatile bool       b = true; BF_ASSERT(BF::Move(b)); }
-	{ const volatile bool b = true; BF_ASSERT(BF::Move(b)); }
+	{ bool                t = true, f = false; BF_ASSERT(t);           BF_VERIFY(t);           BF_ERROR(f);           }
+	{ const bool          t = true, f = false; BF_ASSERT(t);           BF_VERIFY(t);           BF_ERROR(f);           }
+	{ volatile bool       t = true, f = false; BF_ASSERT(t);           BF_VERIFY(t);           BF_ERROR(f);           }
+	{ const volatile bool t = true, f = false; BF_ASSERT(t);           BF_VERIFY(t);           BF_ERROR(f);           }
+	{ bool                t = true, f = false; BF_ASSERT(BF::Move(t)); BF_VERIFY(BF::Move(t)); BF_ERROR(BF::Move(f)); }
+	{ const bool          t = true, f = false; BF_ASSERT(BF::Move(t)); BF_VERIFY(BF::Move(t)); BF_ERROR(BF::Move(f)); }
+	{ volatile bool       t = true, f = false; BF_ASSERT(BF::Move(t)); BF_VERIFY(BF::Move(t)); BF_ERROR(BF::Move(f)); }
+	{ const volatile bool t = true, f = false; BF_ASSERT(BF::Move(t)); BF_VERIFY(BF::Move(t)); BF_ERROR(BF::Move(f)); }
+
+	static_assert(std::is_same_v<decltype(BF_VERIFY(true)), bool>);
+	static_assert(std::is_same_v<decltype(BF_ERROR(true)),  bool>);
+
+	BF_BREAK();
 }
 
 
@@ -39,17 +42,50 @@ TEST(Assert, Good)
 
 	if (false)
 		BF_BREAK();
+
+	BF_VERIFY(true);
+	BF_VERIFY(1 + 1 == 2);
+	BF_VERIFY(True<void, void>::Value);
+	BF_VERIFY(std::initializer_list<int>{1, 2, 3}.size() == 3);
+
+	BF_ERROR(false);
+	BF_ERROR(1 + 1 != 2);
+	BF_ERROR(False<void, void>::Value);
+	BF_ERROR(std::initializer_list<int>{1, 2, 3}.size() != 3);
+
+#if BF_DEBUG
+	EXPECT_EQ(BF_VERIFY(true),  true);
+	EXPECT_EQ(BF_ERROR(false),  false);
+#else
+	EXPECT_EQ(BF_VERIFY(true),  true);
+	EXPECT_EQ(BF_VERIFY(false), false);
+	EXPECT_EQ(BF_ERROR(true),   true);
+	EXPECT_EQ(BF_ERROR(false),  false);
+#endif
 }
 
 
 BF_COMPILE_TIME_TEST()
 {
-	// These won't give the expected error message in Release build. In Release they will compile to ((void)0).
 //	BF_ASSERT();					// [CompilationError-Debug]: ImpAssert::EnsureOneBoolParameter
 //	BF_ASSERT(1);					// [CompilationError-Debug]: ImpAssert::EnsureOneBoolParameter
 //	BF_ASSERT(BF::ImmobileClass());	// [CompilationError-Debug]: ImpAssert::EnsureOneBoolParameter
 //	BF_ASSERT(true, true);			// [CompilationError-Debug]: ImpAssert::EnsureOneBoolParameter
 //	BF_ASSERT(true, "message");		// [CompilationError-Debug]: ImpAssert::EnsureOneBoolParameter
+
+//	BF_BREAK(0);					// [CompilationError]: too many arguments for function-like macro invocation
+
+//	BF_VERIFY();					// [CompilationError]: ImpAssert::EnsureOneBoolParameter
+//	BF_VERIFY(1);					// [CompilationError]: ImpAssert::EnsureOneBoolParameter
+//	BF_VERIFY(BF::ImmobileClass());	// [CompilationError]: ImpAssert::EnsureOneBoolParameter
+//	BF_VERIFY(true, true);			// [CompilationError]: ImpAssert::EnsureOneBoolParameter
+//	BF_VERIFY(true, "message");		// [CompilationError]: ImpAssert::EnsureOneBoolParameter
+
+//	BF_ERROR();						// [CompilationError]: ImpAssert::EnsureOneBoolParameter
+//	BF_ERROR(1);					// [CompilationError]: ImpAssert::EnsureOneBoolParameter
+//	BF_ERROR(BF::ImmobileClass());	// [CompilationError]: ImpAssert::EnsureOneBoolParameter
+//	BF_ERROR(true, true);			// [CompilationError]: ImpAssert::EnsureOneBoolParameter
+//	BF_ERROR(true, "message");		// [CompilationError]: ImpAssert::EnsureOneBoolParameter
 }
 
 
@@ -57,12 +93,30 @@ BF_COMPILE_TIME_TEST()
 {
 //	BF_ASSERT;						// [CompilationError]: undeclared identifier
 //	BF_BREAK;						// [CompilationError]: undeclared identifier
-//	BF_ASSERT(true)					// [CompilationError]: missing ';' before '}'
-//	BF_BREAK()						// [CompilationError]: missing ';' before '}'
+//	BF_VERIFY;						// [CompilationError]: undeclared identifier
+//	BF_ERROR;						// [CompilationError]: undeclared identifier
+
+//	BF_ASSERT(true)					// [CompilationError]: missing ';'
+//	BF_BREAK()						// [CompilationError]: missing ';'
+//	BF_VERIFY(true)					// [CompilationError]: missing ';'
+//	BF_ERROR(false)					// [CompilationError]: missing ';'
 }
 
 
-static constexpr void ConstexprTest()
-{
-	BF_ASSERT(true);
-}
+#if BF_DEBUG
+	constexpr int  BF_DUMMY = [] { BF_ASSERT(true);  return 0; }();
+//	constexpr int  BF_DUMMY = [] { BF_ASSERT(false); return 0; }();		// [CompilationError-Debug]: expression did not evaluate to a constant
+//	constexpr int  BF_DUMMY = [] { BF_BREAK();       return 0; }();		// [CompilationError-Debug]: expression did not evaluate to a constant
+	constexpr bool BF_DUMMY = BF_VERIFY(true);
+//	constexpr bool BF_DUMMY = BF_VERIFY(false);							// [CompilationError-Debug]: expression did not evaluate to a constant
+//	constexpr bool BF_DUMMY = BF_ERROR(true);							// [CompilationError-Debug]: expression did not evaluate to a constant
+	constexpr bool BF_DUMMY = BF_ERROR(false);
+#else
+	constexpr int  BF_DUMMY = [] { BF_ASSERT(true);  return 0; }();
+	constexpr int  BF_DUMMY = [] { BF_ASSERT(false); return 0; }();
+	constexpr int  BF_DUMMY = [] { BF_BREAK();       return 0; }();
+	constexpr bool BF_DUMMY = BF_VERIFY(true);
+	constexpr bool BF_DUMMY = BF_VERIFY(false);
+	constexpr bool BF_DUMMY = BF_ERROR(true);
+	constexpr bool BF_DUMMY = BF_ERROR(false);
+#endif

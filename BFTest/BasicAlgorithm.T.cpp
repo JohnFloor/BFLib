630cb157ca734d0db0eadda74330a693ae0e612d
JohnFloor <171188054+JohnFloor@users.noreply.github.com>
2026-02-18 17:07:41 +0100

    BF::Contains(): Testing operator== with different operand types.

--- BFTest/BasicAlgorithm.T.cpp
+++ BFTest/BasicAlgorithm.T.cpp
@@ -9,9 +9,17 @@
 namespace {
 
 
-struct ValueArchetype {
-	constexpr bool operator==(const ValueArchetype& rightOp) const { return value == rightOp.value; }
-	constexpr bool operator!=(const ValueArchetype&) const = delete;
+struct ValueArchetype1 {
+	constexpr bool operator==(const ValueArchetype1& rightOp) const { return value == rightOp.value; }
+	constexpr bool operator!=(const auto&) const = delete;
+
+	int value;
+};
+
+
+struct ValueArchetype2 {
+	constexpr bool operator==(int rightOp) const { return value == rightOp; }
+	constexpr bool operator!=(const auto&) const = delete;
 
 	int value;
 };
@@ -22,8 +30,8 @@ struct ValueArchetype {
 
 BF_COMPILE_TIME_TEST()
 {
-	using List = std::initializer_list<ValueArchetype>;
-	constexpr ValueArchetype a(1), b(2), c(3);
+	using List = std::initializer_list<ValueArchetype1>;
+	constexpr ValueArchetype1 a(1), b(2), c(3);
 
 	static_assert(!BF::Contains(List{},           a));
 	static_assert( BF::Contains(List{ a },        a));
@@ -39,4 +47,26 @@ BF_COMPILE_TIME_TEST()
 	static_assert(!BF::Contains(List{ a },        c));
 	static_assert(!BF::Contains(List{ a, b },     c));
 	static_assert( BF::Contains(List{ a, b , c }, c));
+}
+
+
+BF_COMPILE_TIME_TEST()
+{
+	using List = std::initializer_list<ValueArchetype2>;
+	constexpr ValueArchetype2 a(1), b(2), c(3);
+
+	static_assert(!BF::Contains(List{},           a.value));
+	static_assert( BF::Contains(List{ a },        a.value));
+	static_assert( BF::Contains(List{ a, b },     a.value));
+	static_assert( BF::Contains(List{ a, b , c }, a.value));
+
+	static_assert(!BF::Contains(List{},           b.value));
+	static_assert(!BF::Contains(List{ a },        b.value));
+	static_assert( BF::Contains(List{ a, b },     b.value));
+	static_assert( BF::Contains(List{ a, b , c }, b.value));
+
+	static_assert(!BF::Contains(List{},           c.value));
+	static_assert(!BF::Contains(List{ a },        c.value));
+	static_assert(!BF::Contains(List{ a, b },     c.value));
+	static_assert( BF::Contains(List{ a, b , c }, c.value));
 }
